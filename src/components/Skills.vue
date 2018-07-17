3df81dbc887ce797db36093bc0da4514f3ef605b
Fawwaz Khayyat <fawaz.khayat@gmail.com>
2018-07-17 19:33:16 -0400

    Add data to skills component. Add button ,and use v-on:click and v-bind:disabled

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="hello">
-    Skills
+    {{ name }}
+
+    <button v-on:click="changeName" v-bind:disabled="btnState">Change Name</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Skills',
-  props: {
-
+  data() {
+    return {
+      name: 'Coursetro',
+      btnState: true
+    }
   }
 }
 </script>
