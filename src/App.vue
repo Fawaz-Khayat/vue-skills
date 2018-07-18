7c87a754f0655028b733d7cd81158275223e63c4
Fawwaz Khayyat <fawaz.khayat@gmail.com>
2018-07-17 20:49:19 -0400

    Change the css of App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,28 @@ export default {
 </script>
 
 <style>
-/**
+  /** style from the written version of the course found at
+   https://coursetro.com/posts/code/136/Vue-CSS-Tutorial---Class-and-Style-Binding
+   **/
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+    width: 50%;
+}
+
+/** original styles
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,9 +47,4 @@ export default {
   margin-top: 60px;
 }
 **/
-
-body {
-  background-color: #eeeeee;
-
-}
 </style>
